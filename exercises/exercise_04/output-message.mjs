15800378c6153b36e5ce1b067a0339d26879a298
Denise Cantos <142037050+CantosCDenise@users.noreply.github.com>
2024-06-22 10:39:26 +0200

    exercise 4

---
+++ exercises/exercise_04/output-message.mjs
@@ -0,0 +1,5 @@
+function outputMessage(message) {
+    console.log(`I have a message for you: ${message}`);
+}
+
+export default outputMessage;
---
+++ exercises/exercise_04/script.mjs
@@ -0,0 +1,2 @@
+import outputMessage from "./output-message.mjs";
+outputMessage("I am a ECMAScript module!")
